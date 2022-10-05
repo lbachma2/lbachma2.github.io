c05b7d5ec0a385f217d719112bac337c8db2329a
Landon Bachman <landonbachman@dhcp-10-7-73-168.dhcp.nd.edu>
2022-10-04 20:49:03 -0400

    After HW Submit

---
+++ buttonPresses.js
@@ -0,0 +1,56 @@
+function cseServ(){
+  document.getElementById("extra-info").style.visibility = "visible";
+  document.getElementById("less-info").style.visibility = "visible";
+}
+function cseServMin(){
+  document.getElementById("extra-info").style.visibility = "hidden";
+  document.getElementById("less-info").style.visibility = "hidden";
+}
+
+function texas(){
+  document.getElementById("extra-info2").style.visibility = "visible";
+  document.getElementById("less-info2").style.visibility = "visible";
+}
+
+function texasMin(){
+  document.getElementById("extra-info2").style.visibility = "hidden";
+  document.getElementById("less-info2").style.visibility = "hidden";
+}
+
+function dinos(){
+  document.getElementById("extra-info3").style.visibility = "visible";
+  document.getElementById("less-info3").style.visibility = "visible";
+}
+
+function dinosMin(){
+  document.getElementById("extra-info3").style.visibility = "hidden";
+  document.getElementById("less-info3").style.visibility = "hidden";
+}
+function passwords(){
+  document.getElementById("extra-info4").style.visibility = "visible";
+  document.getElementById("less-info4").style.visibility = "visible";
+}
+
+function passwordsMin(){
+  document.getElementById("extra-info4").style.visibility = "hidden";
+  document.getElementById("less-info4").style.visibility = "hidden";
+}
+
+function battleship(){
+  document.getElementById("extra-info5").style.visibility = "visible";
+  document.getElementById("less-info5").style.visibility = "visible";
+}
+
+function battleshipMin(){
+  document.getElementById("extra-info5").style.visibility = "hidden";
+  document.getElementById("less-info5").style.visibility = "hidden";
+}
+function emissions(){
+  document.getElementById("extra-info6").style.visibility = "visible";
+  document.getElementById("less-info6").style.visibility = "visible";
+}
+
+function emissionsMin(){
+  document.getElementById("extra-info6").style.visibility = "hidden";
+  document.getElementById("less-info6").style.visibility = "hidden";
+}
